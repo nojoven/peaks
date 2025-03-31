90e859d9dcbe24d0f6990d52c14339f0a609e878
HC <work.cedricjoseph@gmail.com>
2025-03-31 10:36:18 +0200

    chore(api): remove fastapi default endpoints

--- src/main.py
+++ src/main.py
@@ -1,15 +1,5 @@
-from typing import Union
 from fastapi import FastAPI
 from src.api.peaks import router as peaks_router
 
 app = FastAPI()
 app.include_router(peaks_router)
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
