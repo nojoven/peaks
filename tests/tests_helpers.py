21bc885a0b0151fe1bbdd4de3a00d28bd27cde97
HC <work.cedricjoseph@gmail.com>
2025-03-31 10:33:40 +0200

    chore: avoid duplicating

---
+++ tests/tests_helpers.py
@@ -0,0 +1,8 @@
+# Utilisation d'une fonction auxiliaire pour éviter la duplication
+def create_peak_data():
+    return {
+        "name": "Mont Blanc",
+        "lat": 45.8325,
+        "lon": 6.8650,
+        "altitude": 4808
+    }
