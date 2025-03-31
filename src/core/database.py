0a4e908e7b408a181c1219f5e684c45e1cbcb186
HC <work.cedricjoseph@gmail.com>
2025-03-31 08:59:18 +0200

    database: create a session

---
+++ src/core/database.py
@@ -0,0 +1,29 @@
+# src/core/db.py
+
+import logging
+import os
+from dotenv import load_dotenv
+from sqlmodel import create_engine, Session, SQLModel
+from typing import Generator
+
+# Load environment variables
+load_dotenv()
+
+# Connection string
+DATABASE_URL = os.getenv("DEFAULT_DB_URL", None)
+
+# Create engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Create database
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+# Get session DB
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        try:
+            yield session
+        except Exception as e:
+            logging.error("Error during DB session usage: %s", e)
+            raise
