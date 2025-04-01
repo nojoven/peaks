a9e3c2403d118e461a87da615bcd168c0799dfd4
HC <work.cedricjoseph@gmail.com>
2025-04-01 08:51:04 +0200

    refactor: extract the generation of the connection string

--- src/core/database.py
+++ src/core/database.py
@@ -1,39 +1,16 @@
 # src/core/database.py
 
 import logging
-import os
+from src.core.db_helpers import get_db_url
 from dotenv import load_dotenv
 from sqlmodel import create_engine, Session, SQLModel
 from typing import Generator
 
 # Load environment variables
 load_dotenv()
-DATABASE_ENGINE = os.getenv("DATABASE_ENGINE")
-if not DATABASE_ENGINE:
-    raise ValueError("DATABASE_ENGINE is not set in the environment variables.")
-DATABASE_USER = os.getenv("POSTGRES_USER")
-if not DATABASE_USER:
-    raise ValueError("DATABASE_USER is not set in the environment variables.")
-DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-if not DATABASE_PASSWORD:
-    raise ValueError("DATABASE_PASSWORD is not set in the environment variables.")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-if not DATABASE_NAME:
-    raise ValueError("DATABASE_NAME is not set in the environment variables.")
-DATABASE_ADDRESS = os.getenv("DATABASE_ADDRESS")
-if not DATABASE_ADDRESS:
-    raise ValueError("DATABASE_ADDRESS is not set in the environment variables.")
-DATABASE_PORT = os.getenv("DATABASE_PORT")
-if not DATABASE_PORT:
-    raise ValueError("DATABASE_PORT is not set in the environment variables.")
-
-
-# Connection string
-DATABASE_URL = f"{DATABASE_ENGINE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_ADDRESS}:{DATABASE_PORT}/{DATABASE_NAME}"
-
 
 # Create engine
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(get_db_url(), echo=True)
 
 # Create database
 def create_db():
---
+++ src/core/db_helpers.py
@@ -0,0 +1,38 @@
+import os
+
+
+def get_db_url():
+    """
+    Generates a database connection URL based on environment variables.
+    Args: None
+    Returns:
+        str: A formatted database connection URL.
+    Raises:
+        ValueError: If any of the required environment variables is not set.
+    """
+    DATABASE_ENGINE = os.getenv("DATABASE_ENGINE")
+    if not DATABASE_ENGINE:
+        raise ValueError("DATABASE_ENGINE is not set in the environment variables.")
+
+    DATABASE_USER = os.getenv("POSTGRES_USER")
+    if not DATABASE_USER:
+        raise ValueError("DATABASE_USER is not set in the environment variables.")
+
+    DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    if not DATABASE_PASSWORD:
+        raise ValueError("DATABASE_PASSWORD is not set in the environment variables.")
+
+    DATABASE_ADDRESS = os.getenv("DATABASE_ADDRESS")
+    if not DATABASE_ADDRESS:
+        raise ValueError("DATABASE_ADDRESS is not set in the environment variables.")
+
+    DATABASE_PORT = os.getenv("DATABASE_PORT")
+    if not DATABASE_PORT:
+        raise ValueError("DATABASE_PORT is not set in the environment variables.")
+
+    DATABASE_NAME = os.getenv("DATABASE_NAME")
+    if not DATABASE_NAME:
+        raise ValueError("DATABASE_NAME is not set in the environment variables.")
+
+    # Connection string
+    return f"{DATABASE_ENGINE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_ADDRESS}:{DATABASE_PORT}/{DATABASE_NAME}"
