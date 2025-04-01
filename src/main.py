d877984ac5470d1f842795c91998703f2cb34035
HC <work.cedricjoseph@gmail.com>
2025-04-01 13:47:33 +0200

    feat(api): hide swagger in production

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
 from fastapi import FastAPI
 from src.api.peaks import router as peaks_router
-app = FastAPI()
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+IS_PRODUCTION = os.getenv("PRODUCTION", "FALSE").lower() == "true"
+app = FastAPI(
+    title="Peaks API",
+    description="Find data about mountains.",
+    version="1.0",
+    docs_url=None if IS_PRODUCTION else "/docs",  # Disable Swagger UI
+    redoc_url=None if IS_PRODUCTION else "/redoc",  # Disable ReDoc
+    openapi_url=None if IS_PRODUCTION else "/openapi.json"
+)
 app.include_router(peaks_router)
+
+
+# @app.get("/")
+# def read_root():
+#     return
