decd88845ec917151d1ea3d83cd543e248403a72
HC <work.cedricjoseph@gmail.com>
2025-04-06 04:47:32 +0200

    improve

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # entrypoint.sh
 
 echo "Running Alembic migrations..."
-alembic upgrade head
+alembic -c /peaks/alembic.ini upgrade head
 
 echo "Starting API with Uvicorn..."
 exec uvicorn src.main:app --host 0.0.0.0 --port 8000
